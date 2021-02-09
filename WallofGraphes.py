ace9334d3b7b59f39750065a64b56acfe5eedacb
kurokay <49321712+kurokay@users.noreply.github.com>
2021-02-10 00:57:40 +0100

    Update WallofGraphes.py

--- WallofGraphes.py
+++ WallofGraphes.py
@@ -35,6 +35,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+	return render_template("WallofGraphes.html", prix=prix, conso=conso);
 	name_prix_produits = SQL_SELECT(QUERRY_getIdPrixProduits(id_produit_jeu))
 	now = datetime.datetime.now() # Une requête pour avoir le temps SQL serait bien
 	heure = str(now.hour)+":"+str(now.min)
@@ -45,7 +46,7 @@ def index():
 	    new_conso.append(SQL_SELECT(QUERRY_getConsoPeriode(produit['id'])))
 	prix.append(new_prix)
 	conso.append(new_conso)
-    return render_template("WallofGraphes.html", prix=prix, conso=conso);
+    
 
 
 # ---------------------------------------
