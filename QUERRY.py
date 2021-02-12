bc1d8a7eb049dfb1a44d241c482adeddb209b6d2
NathanA14 <nathan.attia@me.com>
2021-02-12 17:55:31 +0100

    update QUERRY.py

--- QUERRY.py
+++ QUERRY.py
@@ -1,37 +1,37 @@
-print("SQL Querry initialization ")
+print("SQL Querry initialization ".format())
 
 ## Le schema de la bdd (attention c'est la nouvelle qui n'a pas été mise en place !!) est dispo dans le dossier pour aider
 
 def QUERRY_getIdPrixProduits():
-    return (f"SELECT id, prix, name FROM produits WHERE id IN {SQLid_produit_jeu} ;")
+    return ("SELECT id, prix, name FROM produits WHERE id IN {0} ;".format(SQLid_produit_jeu))
 
 def QUERRY_getConsoPeriode(IDproduit): #renvoie le nb vendu sur la periode pour UN PRODUIT
     if time_period >= 10:
-        return (f"SELECT count(*) FROM consos WHERE produits_id = {IDproduit} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{'00:'+str(time_period)+':00'}' ;")
+        return ("SELECT count(*) FROM consos WHERE produits_id = {0} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{1}' ;".format(IDproduit,'00:'+str(time_period)+':00'))
     else :
-        return (f"SELECT count(*) FROM consos WHERE produits_id = {IDproduit} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{'00:0'+str(time_period)+':00'}' ;")
+        return ("SELECT count(*) FROM consos WHERE produits_id = {0} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{1}' ;".format(IDproduit,'00:0'+str(time_period)+':00'))
 
 def QUERRY_getConsoTotalePeriode(): #renvoie le nb vendu sur la periode pour TOUS LES PRODUITS en jeu
     if time_period >= 10:
-        return (f"SELECT count(*) FROM consos WHERE produits_id IN {SQLid_produit_jeu} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{'00:'+str(time_period)+':00'}' ;")
+        return ("SELECT count(*) FROM consos WHERE produits_id IN {0} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{1}' ;".format(SQLid_produit_jeu,'00:'+str(time_period)+':00'))
     else:
-        return (f"SELECT count(*) FROM consos WHERE produits_id IN {SQLid_produit_jeu} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{'00:0'+str(time_period)+':00'}' ;")
+        return ("SELECT count(*) FROM consos WHERE produits_id IN {0} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{1}' ;".format(SQLid_produit_jeu,'00:0'+str(time_period)+':00'))
 
 def QUERRY_getConsoPeriodeMoinsUn(IDproduit): #renvoie le nb vendu sur la periode pour UN PRODUIT
     if time_period >= 10 :
-        return (f"SELECT count(*) FROM consos WHERE produits_id = {IDproduit} AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) < '{'00:'+str(2*time_period)+':00'}' AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) > '{'00:'+str(time_period)+':00'}' ; ")
+        return ("SELECT count(*) FROM consos WHERE produits_id = {0} AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) < '{1}' AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) > '{2}' ; ".format(IDproduit,'00:'+str(2*time_period)+':00','00:'+str(time_period)+':00'))
     elif time_period >= 5:
-        return (f"SELECT count(*) FROM consos WHERE produits_id = {IDproduit} AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) < '{'00:'+str(2*time_period)+':00'}' AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) > '{'00:0'+str(time_period)+':00'}' ; ")
+        return ("SELECT count(*) FROM consos WHERE produits_id = {0} AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) < '{1}' AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) > '{2}' ; ".format(IDproduit,'00:'+str(2*time_period)+':00','00:0'+str(time_period)+':00'))
     else :
-        return (f"SELECT count(*) FROM consos WHERE produits_id = {IDproduit} AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) < '{'00:0'+str(2*time_period)+':00'}' AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) > '{'00:0'+str(time_period)+':00'}' ; ")
+        return ("SELECT count(*) FROM consos WHERE produits_id = {0} AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) < '{1}' AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) > '{2}' ; ".format(IDproduit,'00:0'+str(2*time_period)+':00','00:0'+str(time_period)+':00'))
 
 def QUERRY_getConsoTotalePeriodeMoinsUn(): #renvoie le nb vendu sur la periode pour TOUS LES PRODUITS en jeu
     if time_period >= 10:
-        return (f"SELECT count(*) FROM consos WHERE produits_id IN {SQLid_produit_jeu} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{'00:'+str(2*time_period)+':00'}'AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) > '{'00:'+str(time_period)+':00'}' ;")
+        return ("SELECT count(*) FROM consos WHERE produits_id IN {0} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{1}'AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) > '{2}' ;".format(SQLid_produit_jeu,'00:'+str(2*time_period)+':00','00:'+str(time_period)+':00'))
     elif time_period >= 5:
-        return (f"SELECT count(*) FROM consos WHERE produits_id IN {SQLid_produit_jeu} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{'00:'+str(2*time_period)+':00'}'AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) > '{'00:0'+str(time_period)+':00'}' ;")
+        return ("SELECT count(*) FROM consos WHERE produits_id IN {0} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{1}'AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) > '{2}' ;".format(SQLid_produit_jeu,'00:'+str(2*time_period)+':00','00:0'+str(time_period)+':00'))
     else:
-        return (f"SELECT count(*) FROM consos WHERE produits_id IN {SQLid_produit_jeu} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{'00:0'+str(2*time_period)+':00'}'AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) > '{'00:0'+str(time_period)+':00'}' ;")
+        return ("SELECT count(*) FROM consos WHERE produits_id IN {0} AND TIMEDIFF( NOW() , FROM_UNIXTIME(date)) < '{1}'AND TIMEDIFF(NOW(), FROM_UNIXTIME(date)) > '{2}' ;".format(SQLid_produit_jeu,'00:0'+str(2*time_period)+':00','00:0'+str(time_period)+':00'))
 
 def QUERRY_setMontant(ID,montant):
-    return (f"UPDATE produits SET prix={montant} WHERE id={ID};") # Point virgule important ici
+    return ("UPDATE produits SET prix={0} WHERE id={1};".format(montant,ID)) # Point virgule important ici
