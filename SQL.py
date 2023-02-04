ffb86a7083e8d24c6ce58446592fdc67671072e5
Rhadamanthe76 <107307293+Teiwin@users.noreply.github.com>
2023-02-04 18:13:02 +0100

    reformat SQL file

--- SQL.py
+++ SQL.py
@@ -1,19 +1,25 @@
+import mysql.connector
+
+from WallStreetConfig import *
+
+
 def SQL_SELECT(querry):
-    _cnx=mysql.connector.connect(host = host, user = user, password = password, database = database)
-    _cursor=_cnx.cursor()
+    _cnx = mysql.connector.connect(host=host, user=user, password=password, database=database)
+    _cursor = _cnx.cursor()
     _cursor.execute(querry)
-    _select=_cursor.fetchall()
+    _select = _cursor.fetchall()
     _cnx.close()
     return _select
 
+
 def SQL_UPDATE(querry):
-    _cnx=mysql.connector.connect(host = host, user = user, password = password, database = database)
-    _cursor=_cnx.cursor()
-    querrys=querry.split(";")
-    #print("Synchronisation SQL: "+str(len(querrys))+" requetes")
-    n=len(querrys)
+    _cnx = mysql.connector.connect(host=host, user=user, password=password, database=database)
+    _cursor = _cnx.cursor()
+    querrys = querry.split(";")
+    # print("Synchronisation SQL: "+str(len(querrys))+" requetes")
+    n = len(querrys)
     for i in range(len(querrys)):
-        q=querrys[i]
+        q = querrys[i]
         _cursor.execute(q)
         _cnx.commit()
     _cnx.close()
