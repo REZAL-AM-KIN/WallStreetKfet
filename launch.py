fac3e454be628948247d36611ea2c24bba01be03
Rhadamanthe76 <107307293+Teiwin@users.noreply.github.com>
2023-02-04 18:11:58 +0100

    reformat config file

--- launch.py
+++ launch.py
@@ -15,4 +15,12 @@ exec(open(path+'SQL.py').read())
 # time.sleep(1)
 #exec(open(path+'WallofGraphes.py').read())
 #subprocess.call('start /wait python WallofGraphes.py', shell=True)
-exec(open(path+'WallStreetMode.py').read())
+
+# Launch the Flask server
+# launch the main loop
+
+Nb_de_Periodes = int(input("\nCombien de manches de '{0}' minutes voulez vous jouer ?\nAttention ! La partie ne peut être arrêtée en cours et les prix norm'sss kfet seront automatiquement remis à la fin. : \n".format(time_period)))
+print("Le mode WallStreet sera actif pendant '{0}' minutes. NE PAS ETEINDRE LA VM pendant (sinon faudra rechanger les prix modifiés à la main ...)".format(time_period * Nb_de_Periodes))
+
+
+exec(open('/home/justin/Dev_n_Class/WallStreetKfet/WallStreetMode.py').read())
